440479a7f4bcb714c32ad2c659b762865bc52707
eskilschjong <90253352+eskilschjong@users.noreply.github.com>
2025-12-17 11:22:41 +0100

    style: enhanced styling in Explore component

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, CircleUserRound } from "lucide-react";
 
 interface ExploreProps {
     sortBy?: string;
@@ -7,7 +7,7 @@ interface ExploreProps {
 
 export const Explore = ({ sortBy = "Default" }: ExploreProps) => {
     return (
-        <div className="ml-[75vw] h-[70vh] mt-[15vh] p-4 fixed w-[20vw] flex flex-col items-center border-2 border-gray-400 rounded-xl">
+        <div className="ml-[75vw] h-[70vh] mt-[15vh] p-4 fixed w-[20vw] flex flex-col items-center border-l-2 border-gray-300">
             <h1 className="text-xl font-semibold mb-2">Browsing By:</h1>
             <div className="relative inline-block group w-[50%] mb-10">
                 <button className="flex justify-center items-center w-full py-2 bg-[#dae2df] rounded-2xl focus:rounded-b-none capitalize">
@@ -26,12 +26,16 @@ export const Explore = ({ sortBy = "Default" }: ExploreProps) => {
                     </NavLink>
                 </div>
             </div>
+            <div className="font-semibold">Sort by user:</div>
             <div className="overflow-y-scroll flex flex-col h-70 mt-4 w-[70%]
                         [&::-webkit-scrollbar]:w-2
                         [&::-webkit-scrollbar-track]:bg-gray-100
                         [&::-webkit-scrollbar-thumb]:bg-gray-300">
                 {Array.from({ length: 10 }, (_, i) => (
-                    <NavLink to={`/user/${i+1}`} className="border-b border-[#dae2df] py-2 px-8" key={i + 1}>User {i + 1}</NavLink>
+                    <NavLink to={`/user/${i+1}`} className="border-b border-[#dae2df] py-2 px-8 flex flex-row gap-2 font-semibold mb-1 text-[#36423E]" key={i + 1}>
+                        <CircleUserRound />
+                        User {i + 1}
+                    </NavLink>
                 ))}
             </div>
         </div>
